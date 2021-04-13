c6377bbeb44b6e92597f5af366e7536143d6e42d
pauloalmeida0319 <pauloalmeida0319@gmail.com>
2021-04-13 08:57:14 -0300

    13/04/21

---
+++ bikcraft/web/js/main.js
@@ -0,0 +1,6 @@
+$(".rslides").responsiveSlides({
+    auto: true,             // Boolean: Animate automatically, true or false
+    speed: 500,            // Integer: Speed of the transition, in milliseconds
+    timeout: 5000,          // Integer: Time between slide transitions, in milliseconds
+    
+  });
